dcacf6a04f909dd3b60d1b166eaa8dd0d56d4e0d
Matthew Wade <romellem.git@gmail.com>
2017-03-02 11:49:42 -0600

    Creates empty class file
    Will be populated with code shortly

---
+++ src/ExtractSqlTableFromDump.php
@@ -0,0 +1,11 @@
+<?php
+namespace Romellem\ExtractSqlTableFromDump;
+
+/**
+ * Takes a full SQL Database dump (.sql file) and extracts
+ * it into individual table files.
+ */
+class ExtractSqlTableFromDump
+{
+
+}
